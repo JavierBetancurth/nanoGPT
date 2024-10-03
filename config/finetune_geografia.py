3a22a69575a19d52f3e44add4d6a2f6f39607a10
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2024-10-03 15:05:23 -0500

    Update finetune_geografia.py

--- config/finetune_geografia.py
+++ config/finetune_geografia.py
@@ -1,8 +1,8 @@
 import time
 
 out_dir = 'out-geografia'
-eval_interval = 50
-eval_iters = 50
+eval_interval = 10
+eval_iters = 20
 wandb_log = False # feel free to turn on
 wandb_project = 'geografia'
 wandb_run_name = 'ft-' + str(time.time())
@@ -18,7 +18,7 @@ always_save_checkpoint = False
 # shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 1
 gradient_accumulation_steps = 32
-max_iters = 100
+max_iters = 20
 
 # finetune at constant LR
 learning_rate = 3e-5
