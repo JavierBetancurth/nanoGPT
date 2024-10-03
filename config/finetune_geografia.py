57100258e554d0a28c87049adab299210b4e07e4
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2024-10-03 14:28:18 -0500

    Update finetune_geografia.py

--- config/finetune_geografia.py
+++ config/finetune_geografia.py
@@ -8,7 +8,7 @@ wandb_project = 'geografia'
 wandb_run_name = 'ft-' + str(time.time())
 
 dataset = 'geografia'
-init_from = 'gpt2' # this is the largest GPT-2 model
+init_from = 'gpt2-medium' # this is the largest GPT-2 model
 
 # only save checkpoints if the validation loss improves
 always_save_checkpoint = False
@@ -18,7 +18,7 @@ always_save_checkpoint = False
 # shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 1
 gradient_accumulation_steps = 32
-max_iters = 20
+max_iters = 2000
 
 # finetune at constant LR
 learning_rate = 3e-5
